42326278d6a1975ce572a8c149b96e4f7dde3c0e
rohangoyal2018 <rohangoyal2018@gmail.com>
2020-10-17 15:42:22 +0530

    Offset Date Time Added

--- src/main/java/Time.java
+++ src/main/java/Time.java
@@ -1,15 +1,19 @@
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 public class Time {
 	public static void main(String...strings) {
 		Instant instant = Instant.now();
 		Timestamp timestamp = Timestamp.from(instant);
+		OffsetDateTime odt = instant.atOffset(ZoneOffset.of("+06:00"));
 		ZonedDateTime zdt = instant.atZone(ZoneId.of("US/Eastern"));
 		System.out.println("UTC:" + instant);
 		System.out.println("IST:" + timestamp);
 		System.out.println("EDT:" + zdt);
+		System.out.println("Dhaka:" + odt);
 	}
 }
